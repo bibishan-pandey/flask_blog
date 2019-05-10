af2af15c36e98e35ea1349318656a94e8680b4d2
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-05-10 16:22:51 +0545

    configure database and create models for table

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,42 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import SignUpForm, SignInForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c9e0b61b4e5c7f750d66180cdbf893d5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+#create database instance
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default = 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+
+    # 1-many relationship i.e. one user can have many post but one post can have only one author with Post class
+    posts = db.relationship('Post', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(100), nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+
+    # add userid foreign key with table name user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+# dummy data
 posts = [
     {
         'author': 'Bibishan Pandey',
