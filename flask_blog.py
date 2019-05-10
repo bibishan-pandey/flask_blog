9a709d3e752800c526f7e96d8773caa04fe42a39
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-05-10 11:26:50 +0545

    create dummy data for testing in the function and looping through templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,10 +1,31 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Bibishan Pandey',
+        'title': 'First Blog Post',
+        'content': 'First Post Content',
+        'date_posted': 'May 4, 2019'
+    },
+    {
+        'author': 'John Doe',
+        'title': 'Second Blog Post',
+        'content': 'Second Post Content',
+        'date_posted': 'May 5, 2019'
+    },
+    {
+        'author': 'Natsu Dragneel',
+        'title': 'Third Blog Post',
+        'content': 'Third Post Content',
+        'date_posted': 'May 6, 2019'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
