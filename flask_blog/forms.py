a2aa67ce76d46f5b5ef3f70db730e9b030c7ad6e
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-05-10 21:39:43 +0545

    user account and profile picture commit

--- flask_blog/forms.py
+++ flask_blog/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_blog.models import User
@@ -32,3 +34,23 @@ class SignInForm(FlaskForm):
 # after this step setup the secret key in the app
 # good way to generate secret key is to open python interpretor and import secrets and use secrets.token_hex(16)
 # and import these forms from main app and create routes for it
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[Length(min=4, max=20)])
+    email = StringField('Email', validators=[Email()])
+    picture = FileField('Update Profile Picture', validators = [FileAllowed(['png','jpg'])])
+
+    submit = SubmitField('Update')
+
+    # custom validation
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken!')
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            email = User.query.filter_by(email=email.data).first()
+            if email:
+                raise ValidationError('That email is taken!')
